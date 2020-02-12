c4d0cd3e6a783a0b251f6adfe0de157958d9ce29
codedump00 <blazinasis@gmail.com>
2020-02-12 20:25:28 +0545

    added button to buttons

---
+++ src/core/components/button/Button.tsx
@@ -0,0 +1,34 @@
+import React from "react"
+import "./styles.css"
+
+interface Props {
+  type?: "link" | "flat" | "raised";
+  children: string;
+  style?: React.CSSProperties;
+  color?: string;
+  intent?: string;
+}
+
+export const Button = (props: Props): JSX.Element => {
+  const {
+    type = "raised",
+    style = {},
+    children,
+    color,
+    intent,
+  } = props;
+
+  return (
+    <button
+      name={children.toString()}
+      className={type + " button"}
+      style={{
+        ...style,
+        color,
+        border: `2px solid ${intent}`,
+      }}
+    >
+      {children}
+    </button>
+  )
+}
